3c324f2425db3fd6025abeaabe9edb3eb3b6f8d8
Amit Ashkenazy <amit471@gmail.com>
2020-01-09 21:41:38 +0200

    Made Different functions for different "playcards".

--- src/main/java/org/musical_pursuit/services/MusicalPursuitApplication.java
+++ src/main/java/org/musical_pursuit/services/MusicalPursuitApplication.java
@@ -1,5 +1,11 @@
 package org.musical_pursuit.services;
 
+import org.musical_pursuit.services.db.JDBC;
+import org.musical_pursuit.services.src.FactoryPackage.AssociationPlayCardFactory;
+import org.musical_pursuit.services.src.FactoryPackage.MultipleChoicePlayCardFactory;
+import org.musical_pursuit.services.src.FactoryPackage.SingleAnswerPlayCardFactory;
+import org.musical_pursuit.services.src.PlayCardPackage.AssociationPlayCard;
+import org.musical_pursuit.services.src.PlayCardPackage.IPlayCard;
 import org.musical_pursuit.services.src.PlayCardPackage.SingleAnswerPlayCard;
 
 import javax.ws.rs.GET;
@@ -15,17 +21,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Path("/pursuit")
 public class MusicalPursuitApplication {
 
+    JDBC jdbc = new JDBC();
+    SingleAnswerPlayCardFactory singleAnswerPlayCardFactory = new SingleAnswerPlayCardFactory(jdbc);
+    MultipleChoicePlayCardFactory multipleChoicePlayCardFactory = new MultipleChoicePlayCardFactory(jdbc);
+    AssociationPlayCardFactory associationPlayCardFactory = new AssociationPlayCardFactory(jdbc);
+
     @Path("/0")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response returnJson() {
         ObjectMapper mapper = new ObjectMapper();
-        String[] y = {"Alive & Amplified", "Permanent", "Dance Dance"};
-        SingleAnswerPlayCard x = new SingleAnswerPlayCard("In 000000, Billy Talent release the song ______.", "Red Flag",
-                y);
+//        String[] y = {"Alive & Amplified", "Permanent", "Dance Dance"};
+//        SingleAnswerPlayCard x = new SingleAnswerPlayCard("In 000000, Billy Talent release the song ______.", "Red Flag",
+//                y);
+
+        IPlayCard newPlaycard = singleAnswerPlayCardFactory.CreatePlayCard();
         String json = null;
+
         try {
-            json = mapper.writeValueAsString(x);
+            json = mapper.writeValueAsString(newPlaycard);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
@@ -42,12 +56,15 @@ public class MusicalPursuitApplication {
     @Produces(MediaType.APPLICATION_JSON)
     public Response returnJson_2() {
         ObjectMapper mapper = new ObjectMapper();
-        String[] y = {"Alive & Amplified", "Permanent", "Dance Dance"};
-        SingleAnswerPlayCard x = new SingleAnswerPlayCard("In 111111, Billy Talent release the song ______.", "Red Flag",
-                y);
+//        String[] y = {"Alive & Amplified", "Permanent", "Dance Dance"};
+//        SingleAnswerPlayCard x = new SingleAnswerPlayCard("In 111111, Billy Talent release the song ______.", "Red Flag",
+//                y);
+
+        IPlayCard newPlaycard = multipleChoicePlayCardFactory.CreatePlayCard();
         String json = null;
+
         try {
-            json = mapper.writeValueAsString(x);
+            json = mapper.writeValueAsString(newPlaycard);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
@@ -64,12 +81,15 @@ public class MusicalPursuitApplication {
     @Produces(MediaType.APPLICATION_JSON)
     public Response returnJson_3() {
         ObjectMapper mapper = new ObjectMapper();
-        String[] y = {"Alive & Amplified", "Permanent", "Dance Dance"};
-        SingleAnswerPlayCard x = new SingleAnswerPlayCard("In 222222, Billy Talent release the song ______.", "Red Flag",
-                y);
+//        String[] y = {"Alive & Amplified", "Permanent", "Dance Dance"};
+//        SingleAnswerPlayCard x = new SingleAnswerPlayCard("In 222222, Billy Talent release the song ______.", "Red Flag",
+//                y);
+
+        IPlayCard newPlaycard = associationPlayCardFactory.CreatePlayCard();
         String json = null;
+
         try {
-            json = mapper.writeValueAsString(x);
+            json = mapper.writeValueAsString(newPlaycard);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
