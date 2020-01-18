9f774283dc15903a12fd42296e110edcf9962de4
Amit Ashkenazy <amit471@gmail.com>
2020-01-18 16:30:37 +0200

    Added documentation and exception handling.

--- src/main/java/org/musical_pursuit/services/src/FactoryPackage/SingleAnswerPlayCardFactory.java
+++ src/main/java/org/musical_pursuit/services/src/FactoryPackage/SingleAnswerPlayCardFactory.java
@@ -12,10 +12,11 @@ import java.util.List;
 import java.util.Random;
 
 
+// A factory for creating playcard of the single answer type.
 public class SingleAnswerPlayCardFactory implements IPlayCardFactory{
 
-    JDBC jdbc;
-    int totalOptionsNum = 4;
+    JDBC jdbc; // The object used to get info from the database.
+    int totalOptionsNum = 4; // The total number of options.
 
 
     public SingleAnswerPlayCardFactory(JDBC jdbc) {
@@ -27,59 +28,97 @@ public class SingleAnswerPlayCardFactory implements IPlayCardFactory{
     public IPlayCard CreatePlayCard() {
 
         Random randomizer = new Random();
-
-        int about =  randomizer.nextInt(2);
-        int correctPosition = randomizer.nextInt(4);
-
-        HashMap<Integer, List< Song >> answers =  jdbc.getSongsDtl(1, totalOptionsNum - 1);
-
         String question;
         String correctAnswer;
         String[] options = new String[totalOptionsNum];
 
-        Song correctSong = answers.get(0).get(0);
-        Artist correctArtist = correctSong.getArtist();
-        List<Song> incorrect = answers.get(1);
+        while(true) {
+            boolean restartLoop = false;
+            int about =  randomizer.nextInt(2); // Decides whether the question is about a song or about an artist.
+            int correctPosition = randomizer.nextInt(4);
 
+            // Here the jdbc gets all of the options and specifies the correct answer.
+            HashMap<Integer, List< Song >> answers =  jdbc.getSongsDtl(1, totalOptionsNum - 1);
 
-        switch (about) {
-            case 0:
-                question = "Which of the following songs was released by " + correctArtist.getArtistName() + " in " + String.valueOf(correctSong.getYear()) + "?";
-                correctAnswer = correctSong.getTitle();
-                break;
-            case 1:
-                question = "Which of the following artists has released the song " + correctSong.getTitle() + " in " + String.valueOf(correctSong.getYear()) + "?";
-                correctAnswer = correctArtist.getArtistName();
-                break;
-            default:
-                correctAnswer = "no answer";
-                question = "no question";
-                System.out.println("Something is wrong with 'about'");
-        }
+            Song correctSong = answers.get(0).get(0);
+            Artist correctArtist = correctSong.getArtist();
+            List<Song> incorrect = answers.get(1);
 
-        options[correctPosition] = correctAnswer;
+            // build the question and gets the correct answer based on what the question is about.
+            switch (about) {
 
-        switch (about) {
-            case 0:
-                for (int i = 0; i < totalOptionsNum - 1; ++i) {
-                    if (i == correctPosition) {
+                // The question is about a song.
+                case 0:
+                    question = "Which of the following songs was released by " + correctArtist.getArtistName() + " in " + String.valueOf(correctSong.getYear()) + "?";
+                    try {
+                        correctAnswer = correctSong.getTitle();
+                    } catch (NullPointerException e) {
                         continue;
                     }
-                    options[i] = incorrect.get(i).getTitle();
-                }
-                break;
-            case 1:
-                for (int i = 0; i < totalOptionsNum - 1; ++i) {
-                    if (i == correctPosition) {
+                    break;
+
+                // The question is about an artist.
+                case 1:
+                    question = "Which of the following artists has released the song " + correctSong.getTitle() + " in " + String.valueOf(correctSong.getYear()) + "?";
+                    try {
+                        correctAnswer = correctArtist.getArtistName();
+                    } catch (NullPointerException e) {
                         continue;
                     }
-                    options[i] = incorrect.get(i).getArtist().getArtistName();
-                }
-                break;
-            default: System.out.println("Something is wrong with 'about'");
-        }
+                    break;
 
-        return new SingleAnswerPlayCard(question, correctAnswer, options);
+                // Only enters if something went wrong with the jdbc's function of getting the options.
+                default:
+                    correctAnswer = "no answer";
+                    question = "no question";
+                    System.out.println("Something is wrong with 'about'");
+            }
+
+            options[correctPosition] = correctAnswer;
+
+            switch (about) {
+
+                case 0:
+                    for (int i = 0; i < totalOptionsNum - 1; ++i) {
+                        if (i == correctPosition) {
+                            continue;
+                        }
+                        try {
+                            options[i] = incorrect.get(i).getTitle();
+                        } catch (NullPointerException e) {
+                            restartLoop = true;
+                            break;
+                        }
+                    }
+                    if (restartLoop) {
+                        continue;
+                    }
+                    break;
+
+                case 1:
+                    for (int i = 0; i < totalOptionsNum - 1; ++i) {
+                        if (i == correctPosition) {
+                            continue;
+                        }
+                        try {
+                            options[i] = incorrect.get(i).getArtist().getArtistName();
+                        } catch (NullPointerException e) {
+                            restartLoop = true;
+                            break;
+                        }
+                    }
+                    if (restartLoop) {
+                        continue;
+                    }
+                    break;
+
+                default:
+                    System.out.println("Something is wrong with the randomizer.");
+                    continue;
+            }
+
+            return new SingleAnswerPlayCard(question, correctAnswer, options);
+        }
 //        return null;
     }
 }
